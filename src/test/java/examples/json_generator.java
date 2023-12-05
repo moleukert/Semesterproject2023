14403593bfbf4491c0e64f83b143df8cfe780ecb
moleukert <132389463+moleukert@users.noreply.github.com>
2023-12-05 14:13:04 +0100

    first_atttempt for gson
    a first fuzzing attempt with json on the gson library analog to the googleclosure example

---
+++ src/test/java/examples/json_generator.java
@@ -0,0 +1,152 @@
+
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class JsonGenerator extends Generator<String> {
+    public JsonGenerator() {
+        super(String.class);
+    }
+
+    private static final int MAX_DEPTH = 100;
+    private Set<String> indentifiers;
+
+    private int depth;
+
+    @Override
+    public String generate(SourceOfRandomness random, GenerationStatus __status__) {
+        this.depth = 0;
+        this.indentifiers = new HashSet<>();
+        return generate_element(random).toString();
+    }
+
+    public String generate_element(SourceOfRandomness random) {
+        return generate_ws(random) + generate_value(random) + generate_ws(random);
+    }
+
+    public String generate_value(SourceOfRandomness random) {
+        String result;
+
+        result = random.choose(Arrays.<Function<SourceOfRandomness, String>>asList(
+                this::generate_object,
+                this::generate_array,
+                this::generate_string,
+                this::generate_number,
+                this::generate_TRUE,
+                this::generate_FALSE,
+                this::generate_NULL)).apply(random);
+
+        return result;
+    }
+
+    public String generate_object(SourceOfRandomness random) {
+        String result;
+
+        result = random.choose(Arrays.<Function<SourceOfRandomness, String>>asList(
+                this::generate_members,
+                this::generate_ws)).apply(random);
+
+        return '{' + result + '}';
+
+    }
+
+    public String generate_array(SourceOfRandomness random) {
+        String result;
+
+        result = random.choose(Arrays.<Function<SourceOfRandomness, String>>asList(
+                this::generate_elements,
+                this::generate_ws)).apply(random);
+
+        return '[' + result + ']';
+
+    }
+
+    public String generate_string(SourceOfRandomness random) {
+        String result;
+
+        result = generate_characters(random);
+
+        return '"' + result + '"';
+
+    }
+
+    public String generate_number(SourceOfRandomness random) {
+        String result;
+
+        result = generate_integer(random) +
+                generate_fraction(random) +
+                generate_exponent(random);
+
+        return result;
+
+    }
+
+    public String generate_members(SourceOfRandomness random) {
+        String result;
+
+        result = generate_member(random);
+
+        if (random.nextBoolean()) {
+            result += ',' + generate_members(random);
+        }
+        return result;
+    }
+
+    public String generate_member(SourceOfRandomness random) {
+
+        return generate_ws(random) +
+                generate_string(random) +
+                generate_ws(random) + ':' +
+                generate_element(random);
+    }
+
+    public String generate_elements(SourceOfRandomness random) {
+        String result;
+
+        result = generate_element(random);
+
+        if (random.nextBoolean()) {
+            result += ',' + generate_elements(random);
+        }
+        return result;
+    }
+
+    public String generate_element(SourceOfRandomness random) {
+
+        return generate_ws(random) +
+                generate_value(random) +
+                generate_ws(random);
+    }
+
+    public String generate_characters(SourceOfRandomness random) {
+        String result;
+
+        if (random.nextBoolean()) {
+            result = generate_character(random) + generate_characters(random);
+        } else {
+            result = "";
+        }
+        return result;
+    }
+
+    public String generate_character(SourceOfRandomness random) {
+        String result;
+
+        if (random.nextBoolean()) {
+            // generate any char except " or / or cntrl chars
+            result = "";
+        } else {
+            result = "\\" + generate_escape(random);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/examples/json_test.java
@@ -0,0 +1,30 @@
+package examples;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.pholser.junit.quickcheck.From;
+import examples.JSONCodeGeneratorWithMaxRecursionNew;
+import examples.AsciiStringGenerator;
+import edu.berkeley.cs.jqf.fuzz.JQF;
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import org.junit.Assume;
+import org.junit.runner.RunWith;
+
+@RunWith(JQF.class)
+public class JsonTest2 {
+
+    private Gson gson = new Gson();
+
+    @Fuzz
+    public void fuzzJSONParser(@From(JSONCodeGeneratorWithMaxRecursionNew.class) String input) {
+        try {
+            gson.fromJson(input, Object.class);
+        } catch (JsonSyntaxException e) {
+            Assume.assumeNoException(e);
+        } catch (JsonIOException e) {
+            Assume.assumeNoException(e);
+        }
+    }
+}
