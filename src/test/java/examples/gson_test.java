b65d5a42a227c04ae7de87d22e95133cbbbf14e6
moleukert <132389463+moleukert@users.noreply.github.com>
2023-12-05 14:21:33 +0100

    renaming of files and imports

--- src/test/java/examples/gson_test.java
+++ src/test/java/examples/gson_test.java
@@ -5,7 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import com.pholser.junit.quickcheck.From;
-import examples.JSONCodeGeneratorWithMaxRecursionNew;
+import examples.json_generator;
 import examples.AsciiStringGenerator;
 import edu.berkeley.cs.jqf.fuzz.JQF;
 import edu.berkeley.cs.jqf.fuzz.Fuzz;
@@ -18,7 +18,7 @@ public class JsonTest2 {
     private Gson gson = new Gson();
 
     @Fuzz
-    public void fuzzJSONParser(@From(JSONCodeGeneratorWithMaxRecursionNew.class) String input) {
+    public void fuzzJSONParser(@From(json_generator.class) String input) {
         try {
             gson.fromJson(input, Object.class);
         } catch (JsonSyntaxException e) {
