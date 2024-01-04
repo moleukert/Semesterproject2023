ad6f980c30d85c3fd5a39bf0f407337e7fc1a5b8
Anton S <60275084+20anton02@users.noreply.github.com>
2024-01-04 15:20:11 +0100

    Update gson_test.java
    Mehr Tests

--- gson_fuzzing/src/test/java/examples/gson_test.java
+++ gson_fuzzing/src/test/java/examples/gson_test.java
@@ -2,8 +2,11 @@ package examples;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import com.google.gson.ToNumberPolicy;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.JsonParseException;
 import com.pholser.junit.quickcheck.From;
 import examples.JsonGenerator;
 import edu.berkeley.cs.jqf.fuzz.JQF;
@@ -15,9 +18,15 @@ import org.junit.runner.RunWith;
 public class gson_test {
 
     private Gson gson = new Gson();
+    private JsonParser parser = new JsonParser();
+    private Gson custom = new GsonBuilder()
+            .setLenient()
+            .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
+            .create();
 
     @Fuzz
     public void fuzzJSONParser(@From(JsonGenerator.class) String input) {
+        // test standard deserialization with gson
         try {
             gson.fromJson(input, Object.class);
         } catch (JsonSyntaxException e) {
@@ -25,5 +34,20 @@ public class gson_test {
         } catch (JsonIOException e) {
             Assume.assumeNoException(e);
         }
+        // test standard parsing to Tree with JsonParser
+        try {
+            parser.parseString(input);
+        } catch (JsonParseException e) {
+            Assume.assumeNoException(e);
+        }
+        // test deserialization with custom gson
+        try {
+            custom.fromJson(input, Object.class);
+        } catch (JsonSyntaxException e) {
+            Assume.assumeNoException(e);
+        } catch (JsonIOException e) {
+            Assume.assumeNoException(e);
+        }
+
     }
 }
