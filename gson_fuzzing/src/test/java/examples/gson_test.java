d5610fb93343fb99c24647cfacee9ded335470d4
moleukert <132389463+moleukert@users.noreply.github.com>
2024-01-09 15:21:07 +0100

    updated driver
    new tests for mvn testing

--- gson_fuzzing/src/test/java/examples/gson_test.java
+++ gson_fuzzing/src/test/java/examples/gson_test.java
@@ -3,6 +3,7 @@ package examples;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 import com.google.gson.ToNumberPolicy;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
@@ -27,20 +28,40 @@ public class gson_test {
     public void fuzzJSONParser(@From(JsonGenerator.class) String input) {
         // test standard deserialization with gson
         try {
+            // convert input string into object
             Object object = gson.fromJson(input, Object.class);
-            gson.toJson(object);
+
+            // convert object back to string
+            String conv_json = gson.toJson(object);
+
+            // convert object to tree of jsonelements
+            JsonElement json_tree = gson.toJsonTree(object);
+
+            // convert tree back to string
+            String conv_tree = gson.toJson(json_tree);
+
+            // test if the strings are the same (they should be)
+            // assertEquals(conv_json, conv_tree);
+
+            // parse the input string to a parse tree of jsonelements
+            JsonElement parse_tree = JsonParser.parseString(input);
+
+            // convert the parse tree back to a string
+            String conv_parsetree = gson.toJson(parse_tree);
+            parse_tree.toString();
+
+            // System.out.println("input: " + input + "\nConvert: " + conv_json +
+            // "\nConverttree: " + conv_tree
+            // + "\nConvert parse tree: " + conv_parsetree + "\n");
+
         } catch (JsonSyntaxException e) {
             Assume.assumeNoException(e);
         } catch (JsonIOException e) {
             Assume.assumeNoException(e);
-        }
-
-        // test standard parsing to Tree with JsonParser
-        try {
-            JsonParser.parseString(input);
         } catch (JsonParseException e) {
             Assume.assumeNoException(e);
         }
+
         // test deserialization with custom gson
         try {
             gson.fromJson(input, Object.class);
